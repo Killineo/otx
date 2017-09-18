ca929b5c0ff365cd17242792d003d04840e16182
Killineo <mattrovosk@gmail.com>
2017-09-18 18:16:25 -0300

    trabson

--- data/talkactions/lib/talkactions.lua
+++ data/talkactions/lib/talkactions.lua
@@ -1,12 +1,12 @@
--- Prevent spamm commands --
-function checkExhausted(cid, storage, seconds)
-	local v = exhaustion.get(cid, storage)
-	if(not v) then
-		exhaustion.set(cid, storage, seconds)
-	else
-		doPlayerSendTextMessage(cid, MESSAGE_EVENT_DEFAULT, "Please wait " .. v .. " seconds before trying this command again.")
-		return false
+local logFormat = "[%s] %s %s"
+
+function logCommand(player, words, param)
+	local file = io.open("data/logs/" .. player:getName() .. " commands.log", "a")
+	if not file then
+		return
 	end
 
-	return true
+	io.output(file)
+	io.write(logFormat:format(os.date("%d/%m/%Y %H:%M"), words, param):trim() .. "\n")
+	io.close(file)
 end
