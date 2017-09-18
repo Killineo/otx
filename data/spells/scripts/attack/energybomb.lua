01bfdfd6cd43f58219eb6c6920bbf2ba2c63772b
Killineo <mattrovosk@gmail.com>
2017-09-18 18:08:46 -0300

    trabson

--- data/spells/scripts/attack/energybomb.lua
+++ data/spells/scripts/attack/energybomb.lua
@@ -1,6 +1,6 @@
 local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
-setCombatParam(combat, COMBAT_PARAM_CREATEITEM, 1491)
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
+setCombatParam(combat, COMBAT_PARAM_CREATEITEM, ITEM_ENERGYFIELD_PVP)
 
 local area = createCombatArea(AREA_SQUARE1X1)
 setCombatArea(combat, area)
